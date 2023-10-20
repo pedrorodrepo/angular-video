7bb2e619bb3ef5b5c0e7fefdc52d1ef25eb4834a
Pedro Rodrigues <pedrorodrepo@outlook.com>
2023-10-20 03:23:21 -0300

    Media preview on Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-video';
+  constructor(public sanitizer: DomSanitizer) {}
+
+  valid = true;
+  url: any;
+  src: any = null;
+  format: any;
+  selectedFile: any;
+  height: any = 300;
+
+  onSelectFile(event: any) {
+    this.valid = true;
+    const file = (this.selectedFile =
+      event.target.files && event.target.files[0]);
+
+    if (file) {
+      var reader = new FileReader();
+
+      if (file.type.indexOf('image') > -1) {
+        reader.readAsDataURL(file);
+        this.format = 'image';
+      } else if (file.type.indexOf('video') > -1) {
+        reader.readAsDataURL(file);
+        this.format = 'video';
+      } else if (file.type.indexOf('pdf') > -1) {
+        reader.readAsArrayBuffer(file);
+
+        this.format = 'pdf';
+      } else {
+        this.valid = false;
+      }
+
+      reader.onload = (event) => {
+        this.url = (<any>event.target).result;
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+      };
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgIf,
+    MatCardModule,
+    PdfViewerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
